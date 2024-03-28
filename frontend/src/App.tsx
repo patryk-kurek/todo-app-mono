3dff4d0d97fc0556377d4ae8936551a33feef48e
patryk-kurek <patryk.kurek@wpengine.com>
2024-03-28 23:21:47 +0100

    feat: preview

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todo from './components/Todo';
+import { TextField } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Todos</h1>
       </header>
+      <div className="Todo-input">
+        <TextField sx={{backgroundColor: 'white',borderRadius: '10px',width: "100%"}} label="Add Todo" variant='filled'/>
+      </div>
+      <div className="Todo-list">
+        <Todo/>
+        <Todo/>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/components/Todo.tsx
@@ -0,0 +1,27 @@
+import { Card,Box,Fab } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import CheckIcon from '@mui/icons-material/Check';
+
+const Todo = ()=> {
+    return ( 
+        <Card sx={{width: '25%',margin: 'auto',marginTop: '1%'}}>
+            <Box sx={{marginLeft: "50%",marginTop:"2%"}}>
+                <Fab size="small" sx={{position: 'absolute',marginLeft: '3%',marginTop: '0.25%'}}>
+                    <CheckIcon />
+                </Fab>
+                <Fab size="small" sx={{position: 'absolute',marginLeft: '6%',marginTop: '0.25%'}}>
+                    <EditIcon/>
+                </Fab>
+                <Fab size="small" sx={{position: 'absolute',marginLeft: '9%',marginTop: '0.25%'}}>
+                    <DeleteIcon />
+                </Fab> 
+            </Box>
+            <Box sx={{marginLeft: "20px"}}>
+                <h2>todo</h2>
+            </Box>
+        </Card>
+    )
+};
+
+export default Todo;

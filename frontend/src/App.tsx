93b6b43998724cc94653803b3dfa1552ba00a745
patryk-kurek <patryk.kurek@wpengine.com>
2024-03-29 16:24:15 +0100

    feat: adding and deleting todo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,72 @@
 import './App.css';
 import Todo from './components/Todo';
 import { TextField } from '@mui/material';
+import {useState} from "react"
+
+interface Todo { 
+  value: string
+  editMode: boolean
+  completed: boolean
+}
 
 function App() {
+  const [state,setState] = useState<{todos: string[],input: string}>({
+    todos: [],
+    input: ""
+  });
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) : void=>{
+    setState((prevState)=>{
+      return {
+        ...prevState,
+        input: event.target.value
+      }
+    });
+  }; 
+
+  const onDeleteTodo = (index: number) =>{
+    const todos = state.todos;
+    todos.splice(index,1)
+    setState((prevState)=>{
+      return {
+        ...prevState,
+        todos: todos
+      }
+    })
+  };
+  
+  const onSubmitInput = (event: React.KeyboardEvent<HTMLInputElement>)=>{
+    if (event.key == "Enter"){
+      const todos = state.todos;
+      todos.push(state.input)
+       
+      setState((prevState)=>{
+        return {
+          ...prevState,
+          todos: todos,
+          input: ""
+        }
+       });
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Todos</h1>
       </header>
       <div className="Todo-input">
-        <TextField sx={{backgroundColor: 'white',borderRadius: '10px',width: "100%"}} label="Add Todo" variant='filled'/>
+        <TextField 
+          value={state.input} 
+          onChange={onChangeInput} 
+          onKeyDown={onSubmitInput}
+          sx={{backgroundColor: 'white',borderRadius: '10px',width: "100%"}} 
+          label="Add Todo" 
+          variant='filled'/>
       </div>
       <div className="Todo-list">
-        <Todo/>
-        <Todo/>
+        {state.todos.map((todo,index)=>{
+          return <Todo value={todo} onDeleteTodo={()=>onDeleteTodo(index)}/>
+        })}
       </div>
     </div>
   );
--- frontend/src/components/Todo.tsx
+++ frontend/src/components/Todo.tsx
@@ -3,22 +3,22 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import CheckIcon from '@mui/icons-material/Check';
 
-const Todo = ()=> {
+const Todo = (props:any)=> {
     return ( 
         <Card sx={{width: '25%',margin: 'auto',marginTop: '1%'}}>
             <Box sx={{marginLeft: "50%",marginTop:"2%"}}>
                 <Fab size="small" sx={{position: 'absolute',marginLeft: '3%',marginTop: '0.25%'}}>
-                    <CheckIcon />
+                    <CheckIcon/>
                 </Fab>
                 <Fab size="small" sx={{position: 'absolute',marginLeft: '6%',marginTop: '0.25%'}}>
                     <EditIcon/>
                 </Fab>
-                <Fab size="small" sx={{position: 'absolute',marginLeft: '9%',marginTop: '0.25%'}}>
+                <Fab onClick={props.onDeleteTodo} size="small" sx={{position: 'absolute',marginLeft: '9%',marginTop: '0.25%'}}>
                     <DeleteIcon />
                 </Fab> 
             </Box>
             <Box sx={{marginLeft: "20px"}}>
-                <h2>todo</h2>
+                <h2>{props.value}</h2>
             </Box>
         </Card>
     )

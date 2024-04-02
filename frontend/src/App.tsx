4702e8d57297f699bcfcf42adb8143c78a0a2c35
patryk-kurek <patryk.kurek@wpengine.com>
2024-04-02 12:39:36 +0200

    feat: add,edit,delete,complete todo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,89 +1,187 @@
-import './App.css';
-import Todo from './components/Todo';
-import { TextField } from '@mui/material';
-import {useState} from "react"
+import "./App.css";
+import Todo from "./components/Todo";
+import { Modal, TextField, Box, FormControl } from "@mui/material";
+import { useState } from "react";
 
-interface Todo { 
-  value: string
-  editMode: boolean
-  editValue: string
-  completed: boolean
+interface Todo {
+  value: string;
+  completed: boolean;
 }
 
-function App() {
-  const [state,setState] = useState<{todos: Todo[],input: string}>({
-    todos: [],
-    input: ""
-  });
-  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>) : void=>{
-    setState((prevState)=>{
-      return {
-        ...prevState,
-        input: event.target.value
-      }
-    });
-  }; 
+interface EditMode {
+  todoIndex: number | null;
+  value: string;
+}
 
-  const onDeleteTodo = (index: number) =>{
-    const todos = state.todos;
-    todos.splice(index,1)
-    setState((prevState)=>{
+const ModalStyle = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  borderRadius: "10px",
+  boxShadow: 24,
+  p: 4,
+};
+
+function App() {
+  const [state, setState] = useState<{
+    todos: Todo[];
+    editMode: EditMode;
+    input: string;
+  }>({
+    todos: [],
+    editMode: {
+      todoIndex: null,
+      value: "",
+    },
+    input: "",
+  });
+  const onChangeInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setState((prevState) => {
       return {
         ...prevState,
-        todos: todos
-      }
-    })
+        input: event.target.value,
+      };
+    });
   };
-  
-  const onSubmitInput = (event: React.KeyboardEvent<HTMLInputElement>)=>{
-    if (event.key == "Enter"){
+
+  const onDeleteTodo = (index: number) => {
+    const todos = state.todos;
+    todos.splice(index, 1);
+    setState((prevState) => {
+      return {
+        ...prevState,
+        todos: todos,
+      };
+    });
+  };
+
+  const onSubmitInput = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter" && state.input !== "") {
       const todos = state.todos;
       const todo: Todo = {
         value: state.input,
-        editMode: false,
-        editValue: "",
-        completed: false
-      }
-      todos.push(todo)
-       
-      setState((prevState)=>{
+        completed: false,
+      };
+      todos.push(todo);
+
+      setState((prevState) => {
         return {
           ...prevState,
           todos: todos,
-          input: ""
-        }
-       });
+          input: "",
+        };
+      });
     }
   };
 
-  const onCompleteTodo = (index: number)=>{ 
-      const todos = state.todos; 
-      todos[index].completed = true;
-      setState(prevState=>{
-        return {
-          ...prevState, 
-          todos: todos
-        }
-      });
+  const onCompleteTodo = (index: number) => {
+    const todos = state.todos;
+    todos[index].completed = !todos[index].completed;
+    setState((prevState) => {
+      return {
+        ...prevState,
+        todos: todos,
+      };
+    });
   };
-   
+
+  const onClickEditTodo = (index: number) => {
+    setState((prevState) => {
+      return {
+        ...prevState,
+        editMode: {
+          todoIndex: index,
+          value: prevState.todos[index].value,
+        },
+      };
+    });
+  };
+
+  const onChangeInputEditMode = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    setState((prevState) => {
+      return {
+        ...prevState,
+        editMode: {
+          ...prevState.editMode,
+          value: event.target.value,
+        },
+      };
+    });
+  };
+
+  const onSubmitTodoAndExitEditMode = () => {
+    const todos = state.todos;
+    todos[state.editMode.todoIndex as number].value = state.editMode.value;
+    setState((prevState) => {
+      return {
+        ...prevState,
+        // todos: todos,
+        editMode: {
+          todoIndex: null,
+          value: "",
+        },
+      };
+    });
+  };
+
+  const onCloseModal = () => {
+    setState((prevState) => {
+      return {
+        ...prevState,
+        editMode: {
+          todoIndex: null,
+          value: "",
+        },
+      };
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Todos</h1>
       </header>
+      <Modal
+        open={state.editMode.todoIndex != null ? true : false}
+        onClose={onCloseModal}
+      >
+        <Box sx={ModalStyle}>
+          <form onSubmit={onSubmitTodoAndExitEditMode}>
+            <TextField
+              sx={{ width: "100%" }}
+              label="Edit Todo"
+              value={state.editMode.value}
+              onChange={onChangeInputEditMode}
+            />
+          </form>
+        </Box>
+      </Modal>
       <div className="Todo-input">
-        <TextField 
-          value={state.input} 
-          onChange={onChangeInput} 
+        <TextField
+          value={state.input}
+          onChange={onChangeInput}
           onKeyDown={onSubmitInput}
-          sx={{backgroundColor: 'white',borderRadius: '10px',width: "100%"}} 
-          label="Add Todo" 
-          variant='filled'/>
+          sx={{ backgroundColor: "white", borderRadius: "10px", width: "100%" }}
+          label="Add Todo"
+          variant="filled"
+        />
       </div>
       <div className="Todo-list">
-        {state.todos.map((todo,index)=>{
-          return <Todo todo={todo} onCompleteTodo={()=>onCompleteTodo(index)} onDeleteTodo={()=>onDeleteTodo(index)}/>
+        {state.todos.map((todo, index) => {
+          return (
+            <Todo
+              todo={todo}
+              onClickEditTodo={() => onClickEditTodo(index)}
+              onCompleteTodo={() => onCompleteTodo(index)}
+              onDeleteTodo={() => onDeleteTodo(index)}
+            />
+          );
         })}
       </div>
     </div>
